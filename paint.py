aab66fb475bdd0e29f94999d0dc6014bfb9e250d
Rosa <A01748086@tec.mx>
2023-05-10 10:16:51 -0600

    Triangulo

--- paint.py
+++ paint.py
@@ -59,6 +59,16 @@ def rectangle(start, end):
 
 def triangle(start, end):
     """Draw triangle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
 
 
 def tap(x, y):
