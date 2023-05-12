0cb0c68c01e6354a86f5dd87b0dee5dfcdbb75d4
noakim03 <a01747512@tec.mx>
2023-05-12 11:42:32 -0600

    Comentarios agregados

--- memory.py
+++ memory.py
@@ -26,6 +26,7 @@ def square(x, y):
     down()
     color('black', 'white')
     begin_fill()
+    # dibuja los lados del cuadro
     for count in range(4):
         forward(50)
         left(90)
@@ -45,18 +46,27 @@ def xy(count):
 def tap(x, y):
     """Update mark and hidden tiles based on tap."""
     spot = index(x, y)
+
+    # Se actualiza la marca y los tiles ocultos
     mark = state['mark']
     taps.append(1)
     print('Taps:', len(taps))
     title(len(taps))
+    # Si las en las posiciones correspondientes no son iguales,
     if mark is None or mark == spot or letters[mark] != letters[spot]:
+        #     
         state['mark'] = spot
     else:
+        # si son iguales se revelan los tiles ocultos
         hide[spot] = False
         hide[mark] = False
+        # la marca actual se restablece como 'None'
         state['mark'] = None
+        # se añade 1 al final de la lista disp
         disp.append(1)
+        # se imprime la longitud de la lista disp para ver el progreso del juego
         print(len(disp))
+        # si la longitud de disp es igual a 32, se muestra el mensaje en la ventana
         if len(disp) == 32:
             title('LO LOGRASTE')
 
@@ -66,19 +76,27 @@ def draw():
     """Draw image and tiles."""
     clear()
     goto(0, 0)
+    # se dibuja la imagen del coche
     shape(car)
     stamp()
 
+    # se dibujan los tiles ocultos
     for count in range(64):
+        # si el tile está oculto
         if hide[count]:
+            # obtiene las coordenadas del tile oculto
             x, y = xy(count)
+            # y se dibuja el cuadro
             square(x, y)
 
     mark = state['mark']
 
+    # comprueba si hay una marca seleccionada y si el tile correspondiente está oculto
     if mark is not None and hide[mark]:
+        # se obtiene las coordenadas (x,y) del tile seleccionado
         x, y = xy(mark)
         up()
+        # contenido dentro del cuadro, en este caso son las letras del abecedario
         goto(x + 27, y)
         color('black')
         write(letters[mark], align="center", font=('Arial', 30, 'normal'))
