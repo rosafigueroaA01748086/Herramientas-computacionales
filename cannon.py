8a846df487a0e1191812b2280ef07c7eb615cfb5
Rosa <A01748086@tec.mx>
2023-05-12 13:14:37 -0600

    Comentarios finales

--- cannon.py
+++ cannon.py
@@ -1,82 +1,86 @@
-from random import randrange
+from random import *
 from turtle import *
-from freegames import vector
 
-ball = vector(-400, -400)
-speed = vector(0, 0)
-targets = []
+from freegames import path
+
+car = path('car.gif')
+tiles = list(range(2)) * 2
+state = {'mark': None}
+hide = [True] * 4
+taps = []
+disp = []
+
+def square(x, y):
+    """Función que dibuja un cuadro blanco con un contorno negro en la coordenada marcada. Requiere de input dos parámetros x y y."""
+    up()
+    goto(x, y)
+    down()
+    color('black', 'white')
+    begin_fill()
+    for count in range(4):
+        forward(50)
+        left(90)
+    end_fill()
+
+def index(x, y):
+    """Función que convierte las coordenadas en los tiles index. Requiere dos parametros uno de x y otro de y."""
+    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+
+def xy(count):
+    """Función que convierte los tiles en coordenadas. Requiere solo de un parámetro."""
+    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
 def tap(x, y):
-    "Respond to screen tap."
-    if not inside(ball):
-        # Comprobar si la pelota está fuera de la pantalla
-        ball.x = -199
-        ball.y = -199
-        speed.x = (x + 300) / 25
-        speed.y = (y + 300) / 25
-
-def inside(xy):
-    "Return True if xy within screen."
-    return -200 < xy.x < 200 and -200 < xy.y < 200
-
+    """Funcion encargada en actualizar los tiles marcadas y ocultos al tocar"""
+    spot = index(x, y)
+    mark = state['mark']
+    taps.append(1)
+    print('Taps:', len(taps))
+    title(len(taps))
+    if mark is None or mark == spot or tiles[mark] != tiles[spot]:
+        state['mark'] = spot
+    else:
+        hide[spot] = False
+        hide[mark] = False
+        state['mark'] = None
+        disp.append(1)
+        print(len(disp))
+        if len(disp) == 32:
+            title('LO LOGRASTE')
+    
 def draw():
-    "Draw ball and targets."
+    """Función que dibuja los tiles y la imagen. No requiere ningún parámetro."""
     clear()
+    goto(0, 0)
+    shape(car)
+    stamp()
 
-    # Se dibuja el objeto
-    for target in targets:
-        goto(target.x, target.y)
-        dot(20, 'blue')
+    for count in range(4):
+        if hide[count]:
+            x, y = xy(count)
+            square(x, y)
 
-    # Se dibuja la pelota
-    if inside(ball):
-        goto(ball.x, ball.y)
-        dot(6, 'red')
+    mark = state['mark']
 
-    # Se actualiza la pantalla
+    if mark is not None and hide[mark]:
+        x, y = xy(mark)
+        up()
+        goto(x + 2, y)
+        color('black')
+        write(tiles[mark], font=('Arial', 30, 'normal'))
+
+    
+    write('Taps:', font=('Arial', 30, 'normal'))
     update()
+    ontimer(draw, 100)
 
-def move():
-    "Move ball and targets."
 
-    # Se generan los objetos aleatoria
-    if randrange(40) == 0:
-        y = randrange(-150, 150)
-        target = vector(200, y)
-        targets.append(target)
-
-    # Movimiento de los objetos que ya se encuentran dentro de la pantalla
-    for target in targets:
-        target.x -= 2.5
-
-    # Movimiento de la pelota
-    if inside(ball):
-        speed.y -= 0.5
-        ball.move(speed)
-
-    # Colisión entre la pelota y los objetos
-    dupe = targets.copy()
-    targets.clear()
-
-    for target in dupe:
-        # Si la distancia entre el objeto y la pelota es mayor que 13, 
-        # no se ocurre la colisión y el objeto se agrega de nuevo a la lista
-        if abs(target - ball) > 13:
-            targets.append(target)
-
-    draw()
-
-    # Reaparecen los objetos que se salieron de la pantalla
-    for target in targets:
-        if not inside(target):
-            target.x = 200
-
-    ontimer(move, 50)
-
+# Funcion que mezcla el memorama
+shuffle(tiles)
 setup(420, 420, 370, 0)
+addshape(car)
 hideturtle()
-up()
 tracer(False)
 onscreenclick(tap)
-move()
+draw()
 done()
